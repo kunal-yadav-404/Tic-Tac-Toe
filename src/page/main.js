d0de1db50aa659cfe7ca599f039faf2fc54319cb
Kunal Yadav <yadavkunal780@gmail.com>
2023-01-31 12:23:32 +0530

    Initialize project using Create React App

---
+++ src/page/main.js
@@ -0,0 +1,12 @@
+import "../components/style/main.css";
+import "../components/Game.js";
+
+function Main() {
+  return (
+    <>
+      <div id="container"></div>
+    </>
+  );
+}
+
+export default Main;
